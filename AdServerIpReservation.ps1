673f2821ff43912c7b5de847cfa7285c26ff2475
Raphael Büchi <raphael.buechi@hotmail.com>
2020-05-25 18:57:09 +0200

    Added Servers

--- AdServerIpReservation.ps1
+++ AdServerIpReservation.ps1
@@ -1,4 +1,9 @@
 $Servers = Get-AdComputer -filter * | where {$_.OperatingSystem -like "*server*"}
 foreach ($Server in $Servers) {
     # Do some Stuff
+    Get-DhcpServerv4Scope
+    if (Get-DhcpServerv4ExclusionRange -eq null) {
+        Add-DhcpServer4ExclusionRange
+    }
+    
 }
