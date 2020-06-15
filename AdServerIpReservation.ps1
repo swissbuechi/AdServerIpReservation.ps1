93363d5a067ee1161ef1054e2403b83ab99455de
unknown <Administrator@CONTOSO.DOMAIN>
2020-06-15 17:58:25 +0200

    Commit

--- AdServerIpReservation.ps1
+++ AdServerIpReservation.ps1
@@ -1,27 +1,28 @@
 $DhcpExcludedScope = Get-DhcpServerv4ExclusionRange
 if ($null -eq $DhcpExcludedScope) {
-    Write-Host "There is currently no IPv4 DHCP excluded range configured"
+    Write-Host "There is currently no IPv4 DHCP excluded range configured`n"
     $DhcpScopes = Get-DhcpServerv4Scope
-    Write-Host "This is your current DHCP-Range:"
+    Write-Host "This is your current DHCP-Range:`n"
     $DhcpScopes
 
     $continue = Read-Host "Would you like to configure an excluded Range now? `n(yes, y to continue or no, n to cancle)"
     if (($continue -eq "y") -or ($continue -eq "yes")) {
-        $ScopeId = Read-Host "Scope Id: "
-        $StartRange = Read-Host "Start IP-Address: "
-        $EndRange = Read-Host "End IP-Address: "
-        try {
-            Add-DhcpServerv4ExclusionRange -ScopeId $ScopeId -StartRange $StartRange -EndRange $EndRange
+        $DhcpScopesCount = $DhcpScopes | Measure-Object
+        if ($DhcpScopesCount.Count -eq 1) {
+            $ScopeId = $DhcpScopes.ScopeId.IPAddressToString
+        } else {
+            $ScopeId = Read-Host "Scope Id"
         }
-        catch {
-            Write-Host "Could not create your new DCHP excluded Range: start $StartRange end: $EndRange"
-            Write-Host "Please check if your start and end IP-Address is inside your DHCP Scope:"
-            $DhcpScopes
-        }
+        $StartRange = Read-Host "Start IP"
+        $EndRange = Read-Host "End IP"
+        Add-DhcpServerv4ExclusionRange -ScopeId $ScopeId -StartRange $StartRange -EndRange $EndRange
+    } else {
+        exit
     }
 }
+    Write-Host "Test"
 
 
-#$Servers = Get-AdComputer -filter * | where {$_.OperatingSystem -like "*server*"}
-#foreach ($Server in $Servers) {
-#}
+$Servers = Get-AdComputer -filter * | where {$_.OperatingSystem -like "*server*"}
+foreach ($Server in $Servers) {
+}
