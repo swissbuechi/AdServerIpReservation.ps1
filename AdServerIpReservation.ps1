e3dbc387c0b902c0eb5cc52d31b0ca0e0b48b78d
Raphael Büchi <raphael.buechi@hotmail.com>
2020-06-15 16:43:42 +0200

    Commit

--- AdServerIpReservation.ps1
+++ AdServerIpReservation.ps1
@@ -1,14 +1,10 @@
-$Servers = Get-AdComputer -filter * | where {$_.OperatingSystem -like "*server*"}
-foreach ($Server in $Servers) {
-    # Do some Stuff
-    Get-DhcpServerv4Scope
     if (Get-DhcpServerv4ExclusionRange -eq null) {
         Write-Host "There is currently no IPv4 DHCP excluded range configured"
-        Write-Host "This is your current DHCP-Range:" Get-DhcpServerv4Scope
-        Write-Host 
-
-        $confirm = Read-Host "Would you like to configure an excluded Range now? `n(Yes, Y to confirm or No, N to cancle)"
-        if (($confirm.ToLower -eq "y") -or ($confirm.ToLower -eq "yes")) {
+        $DhcpScope = Get-DhcpServerv4Scope
+        Write-Host "This is your current DHCP-Range:" $DhcpScope
+    
+        $continue = Read-Host "Would you like to configure an excluded Range now? `n(Yes, Y to continue or No, N to cancle)"
+        if (($continue.ToLower -eq "y") -or ($continue.ToLower -eq "yes")) {
             Write-Host "Please enter the start IP-Address of your new DHCP excluded Range"
             $StartRange = Read-Host
             Write-Host "Please enter the end IP-Address of your new DHCP excluded Range"
@@ -18,14 +14,18 @@ foreach ($Server in $Servers) {
                 Add-DhcpServerv4ExclusionRange -StartRange $StartRange -EndRange $EndRange
             }
             catch {
-                
+                Write-Host "Could not create your new DCHP excluded Range: start $StartRange end: $EndRange"
+                Write-Host "Please check if your start and end IP-Address is inside your DHCP Scope: $DhcpScope"
             }
 
         }
 
-        if ($confirm -eq )
-
-        Add-DhcpServer4ExclusionRange
+        if (($continue.ToLower -eq "n") -or ($continue.ToLower -eq "no")) {
+            exit
+        }
     }
-    
-}
+
+
+#$Servers = Get-AdComputer -filter * | where {$_.OperatingSystem -like "*server*"}
+#foreach ($Server in $Servers) {
+#}
